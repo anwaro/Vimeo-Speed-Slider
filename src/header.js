38f89e79c7925291babcf359b4d5f38fdc9fabb3
Łukasz Micał <lucas@uigstudio.com>
2023-12-05 13:44:57 +0100

    Bump version

--- src/header.js
+++ src/header.js
@@ -1,7 +1,7 @@
 const header = `// ==UserScript==
 // @name         Vimeo Player Speed Slider
 // @namespace    vimeo_player_speed_slider
-// @version      0.2.1
+// @version      0.3.0
 // @description  Add Speed Slider to Vimeo Player Settings
 // @author       Łukasz
 // @include      https://*.vimeo.com/*
